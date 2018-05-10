21b9f6641f4d62bb407cba2f21156f4bd9865bf2
Havelis <havelis60@gmail.com>
2018-05-10 11:02:24 +0200

    studnet endpoint

--- plugins/data-sources.js
+++ plugins/data-sources.js
@@ -1,6 +1,7 @@
 const DATASOURCES = Object.freeze({
   microbiti: 'http://b2015slavma.delta-studenti.cz/wordpress',
-  koducup: 'http://b2015kopejo.delta-studenti.cz/api'
+  koducup: 'http://b2015kopejo.delta-studenti.cz/api',
+  studnet: 'http://b2015janyja.delta-studenti.cz/wordpress/',
 });
 
 export default DATASOURCES
