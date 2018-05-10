0bb5a1480672170815a8901e71d5286a1922377f
Havelis <havelis60@gmail.com>
2018-05-10 09:59:28 +0200

    add koducup datasource

--- plugins/data-sources.js
+++ plugins/data-sources.js
@@ -1,5 +1,6 @@
 const DATASOURCES = Object.freeze({
-  microbiti: 'http://b2015slavma.delta-studenti.cz/wordpress'
+  microbiti: 'http://b2015slavma.delta-studenti.cz/wordpress',
+  koducup: 'http://b2015kopejo.delta-studenti.cz/api'
 });
 
 export default DATASOURCES
