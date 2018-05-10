b1fd08837fc6008b84ba37bde114b11557d12d03
Havelis <havelis60@gmail.com>
2018-05-10 12:28:24 +0200

    roborave endpoint

--- plugins/data-sources.js
+++ plugins/data-sources.js
@@ -1,7 +1,8 @@
 const DATASOURCES = Object.freeze({
   microbiti: 'http://b2015slavma.delta-studenti.cz/wordpress',
   koducup: 'http://b2015kopejo.delta-studenti.cz/api',
-  studnet: 'http://b2015janyja.delta-studenti.cz/wordpress/',
+  studnet: 'http://b2015janyja.delta-studenti.cz/wordpress',
+  roborave: 'http://b2015zapolu.delta-studenti.cz/roborave/wp-json/spc/v1/api',
 });
 
 export default DATASOURCES
