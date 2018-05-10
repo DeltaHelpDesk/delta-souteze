64145e8d4a1ba0c83911a4b302a88ddef4cc1b55
Unknown <tomaselsnic@seznam.cz>
2018-05-10 14:31:22 +0200

    Fix menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,12 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <nuxt-link to="/" class="navbar-brand js-scroll-trigger">Logo</nuxt-link>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
+        <nuxt-link to="/" class="navbar-brand js-scroll-trigger">HomePage</nuxt-link>
+        <button class="navbar-toggler navbar-toggler-right" type="button" @click="btnClicked">
           Menu
           <i class="fa fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="collapse navbar-collapse" id="navbarResponsive" ref="menu">
           <ul class="navbar-nav text-uppercase ml-auto">
             <li class="nav-item" v-for="(competition, index) in DATASOURCES" :key="index">
               <nuxt-link class="nav-link" :to="`/souteze/${competition}`">{{competition}}</nuxt-link>
@@ -37,7 +35,24 @@ import DATASOURCES from '~/plugins/data-sources';
 export default {
   data: () => ({
     DATASOURCES: Object.keys(DATASOURCES),
+    menuActive: false,
   }),
+  methods: {
+    btnClicked() {
+      if(this.menuActive)
+      {
+        this.menuActive=false;
+       this.$refs['menu'].style="display:none;";
+      }
+      else
+      {
+        this.menuActive=true;
+         this.$refs['menu'].style="display:block;";
+      }
+
+      console.log(this.$refs['menu'])
+    }
+  }
 };
 </script>
 <style>
